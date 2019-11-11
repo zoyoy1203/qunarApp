c0e66ceb7e682165cbbd3ad33305eb6d7fdbe01f
zoyoy1203 <2071686148@qq.com>
2019-11-11 16:25:28 +0800

    景点详情页图片展示

--- src/components/addr-detail/addr-detail.vue
+++ src/components/addr-detail/addr-detail.vue
@@ -2,8 +2,13 @@
   <div class="container">
     <div class="top">
       <back-head class="header"></back-head>
-      <img :src="addrDetail.imgbg" alt="">
-      <div class="title">{{addrDetail.title}}</div>
+      <div class="img_content">
+        <img :src="addrDetail.imgbg" alt="">
+        <div class="title">
+          {{addrDetail.title}}
+          <span class="imgs" @click="showImgs()">图片点击 <i>{{imgs.length}}</i></span>
+        </div>
+      </div>
       <div class="desc">
         <div class="desc_l">
           <div class="desc_ll">
@@ -55,6 +60,21 @@
         </li>
       </ul>
     </div>
+
+    <div class="img-wrapper"
+         @click="imgsActive = false"
+         v-if="imgsActive"
+    >
+      <div class="slider-wrapper" ref="sliderWrapper">
+        <slider>
+          <div v-for="(item,index) in imgs" :key="index">
+            <a href="#">
+              <img class="needsclick"  :src="item">
+            </a>
+          </div>
+        </slider>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -62,15 +82,32 @@
   import BackHead from 'base/back-head/back-head'
   import { getAddrDetail} from 'api/api'
   import { mapGetters } from 'vuex'
+  import Slider from 'base/slider/slider'
   export default {
     data(){
       return {
         addrDetail:[],
-        baseinfo:{}
+        baseinfo:{},
+        imgs:[
+          'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_350x240_8e61302a.jpg',
+          'http://img1.qunarzz.com/sight/p0/1909/4e/4e1afc2e1133ade7a3.img.jpg_350x240_2646c984.jpg',
+          'http://img1.qunarzz.com/sight/p0/1907/ca/ca68bd95bc36bdba3.img.jpg_350x240_0d9e7931.jpg',
+          'http://img1.qunarzz.com/sight/p0/1907/e4/e4a734b0c3fb9598a3.img.jpg_350x240_e0ec48d1.jpg',
+          'http://img1.qunarzz.com/sight/p0/1907/cd/cdd8448b61cd6042a3.img.jpg_350x240_9bd65569.jpg',
+          'http://img1.qunarzz.com/sight/p55/201211/04/fbcab3e5d6479ce893835fbb.jpg_350x240_df8ba9f7.jpg',
+          'http://img1.qunarzz.com/wugc/p180/201306/16/7f08e81624346b1693835fbb.jpg_350x240_b09a9503.jpg',
+          'http://img1.qunarzz.com/wugc/p123/201211/19/a2045d091f02b25493835fbb.png_350x240_cf4cd08e.png',
+          'http://img1.qunarzz.com/sight/p0/1410/e3/73da8d3e19cdc41c1932d4fcd22ec792.water.jpg_350x240_af846382.jpg',
+          'http://img1.qunarzz.com/sight/p0/1410/9d/fe8109ab5df1c9c324e74284fa802e72.water.jpg_350x240_8571d9ed.jpg',
+          'http://img1.qunarzz.com/wugc/p238/201306/16/d9090c728fed64eb93835fbb.jpg_350x240_afe6c0fa.jpg',
+          'http://img1.qunarzz.com/sight/p0/1501/af/af47583adca3f972.water.jpg_350x240_95007698.jpg'
+        ],
+        imgsActive:false,
       }
     },
     components:{
-      BackHead
+      BackHead,
+      Slider
     },
     created () {
       this._getAddrDetail()
@@ -81,6 +118,9 @@
       ])
     },
     methods:{
+      showImgs() {
+        this.imgsActive = true;
+      },
       _getAddrDetail(){
         getAddrDetail(this.addrId).then(res => {
           this.addrDetail = res.data.addrDetail[0]
@@ -111,17 +151,23 @@
     .header{
       color: #fff
     }
-    img{
-      width: 100%
-      height:auto
+    .img_content{
+      img{
+        width: 100%
+        height:auto
+      }
+      .title{
+        position: relative
+        top: -60px
+        color: #fff
+        padding-left: 10px
+        font-size:$icon-font-size-m
+        .imgs{
+          font-size:$font-size-s
+        }
+      }
     }
-    .title{
-      position: relative
-      top: -60px
-      color: #fff
-      padding-left: 10px
-      font-size:$icon-font-size-m
-    }
+
     .desc{
       padding: 0 2%
       display: flex
@@ -238,6 +284,18 @@
           }
         }
       }
+    }
+  }
+  .img-wrapper{
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    background: #000
+    .slider-wrapper{
+      z-index:999;
+      margin-top:300px;
     }
   }
 }
